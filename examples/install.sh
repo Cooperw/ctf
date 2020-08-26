c2cc17e7134363e46a1dcfd055f9b0a63e4076d6
Cooper Wiegand <cooperww32@gmail.com>
2020-08-25 22:07:28 -0400

    Removed old files

--- examples/install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install python3 python3-dev python3-pip python python-dev python-pip build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev libgmp-dev libmpfr-dev libmpc-dev
-
-pip install gmpy2 numpy pwntools pycrypto
-pip3 install gmpy2 numpy pwntools pycrypto
